e07585c66c22e68f5d9d307b5495c4f934146177
Balazs Szabo <balazs@eegl.co>
2016-04-23 11:53:12 +0200

    bugfixes

--- eegl-ng-seo.js
+++ eegl-ng-seo.js
@@ -1,81 +1,84 @@
-'use strict'
+(function() {
+  'use strict'
 
-/*
- * eegl-ng-seo
- *
+  /*
+   * eegl-ng-seo
+   *
 
- * Version: 0.1.2
- * License: MIT
- */
+   * Version: 0.1.3
+   * License: MIT
+   */
 
-angular.module('eegl-ng-seo', [])
-  .provider('PageTitle', function PageTitleProvider () {
-    var title = 'Loading site...'
-    var globalTitle = ''
-    var position
-    var seperator = ' | '
+  angular.module('eegl-ng-seo', [])
+    .provider('PageTitle', function PageTitleProvider () {
+      var title = 'Loading site...'
+      var globalTitle = ''
+      var position
+      var seperator = ' | '
 
-    this.config = function (settings) {
-      if( settings.position )     { position = settings.position }
-      if( settings.seperator )    { seperator = settings.seperator }
-      if( settings.globalTitle )  { globalTitle = settings.globalTitle }
-    }
-
-    this.$get = function PageTitleFactory() {
-      return {
-        title: function() { return title; },
-        setTitle: function(newTitle) {
-          if( position === 'prefix' ) {
-            title = globalTitle + seperator + newTitle
-          } else {
-            title = newTitle + seperator + globalTitle
-          }
-        },
-        setGlobalTitle: function(title) { globalTitle = title }
+      this.config = function (settings) {
+        if( settings.position )     { position = settings.position }
+        if( settings.seperator )    { seperator = settings.seperator }
+        if( settings.globalTitle )  { globalTitle = settings.globalTitle }
       }
-    }
-  })
-  .service('MetaInformation', function() {
-    var metaDescription = ''
-    var metaKeywords = ''
 
-    function reset () {
-      metaDescription = ''
-      metaKeywords = ''
-    }
-
-    function getMetaDescription () {
-      return metaDescription
-    }
-
-    function getMetaKeywords () {
-      return metaKeywords
-    }
-
-    function setMetaDescription (newMetaDescription) {
-      metaDescription = newMetaDescription
-    }
-
-    function setMetaKeywords (newKeywords) {
-      metaKeywords = newKeywords
-    }
-
-    function appendMetaKeywords (newKeywords) {
-      for (var key in newKeywords) {
-        if (metaKeywords === '') {
-          metaKeywords += newKeywords[key].name
-        } else {
-          metaKeywords += ', ' + newKeywords[key].name
+      this.$get = function PageTitleFactory() {
+        return {
+          config: this.config,
+          title: function() { return title; },
+          setTitle: function(newTitle) {
+            if( position === 'prefix' ) {
+              title = globalTitle + seperator + newTitle
+            } else {
+              title = newTitle + seperator + globalTitle
+            }
+          },
+          setGlobalTitle: function(title) { globalTitle = title }
         }
       }
-    }
+    })
+    .service('MetaInformation', function() {
+      var metaDescription = ''
+      var metaKeywords = ''
 
-    return {
-      getMetaDescription: getMetaDescription,
-      getMetaKeywords: getMetaKeywords,
-      reset: reset,
-      setMetaDescription: setMetaDescription,
-      setMetaKeywords: setMetaKeywords,
-      appendMetaKeywords: appendMetaKeywords
-    }
-  })
+      function reset () {
+        metaDescription = ''
+        metaKeywords = ''
+      }
+
+      function getMetaDescription () {
+        return metaDescription
+      }
+
+      function getMetaKeywords () {
+        return metaKeywords
+      }
+
+      function setMetaDescription (newMetaDescription) {
+        metaDescription = newMetaDescription
+      }
+
+      function setMetaKeywords (newKeywords) {
+        metaKeywords = newKeywords
+      }
+
+      function appendMetaKeywords (newKeywords) {
+        for (var key in newKeywords) {
+          if (metaKeywords === '') {
+            metaKeywords += newKeywords[key].name
+          } else {
+            metaKeywords += ', ' + newKeywords[key].name
+          }
+        }
+      }
+
+      return {
+        getMetaDescription: getMetaDescription,
+        getMetaKeywords: getMetaKeywords,
+        reset: reset,
+        setMetaDescription: setMetaDescription,
+        setMetaKeywords: setMetaKeywords,
+        appendMetaKeywords: appendMetaKeywords
+      }
+    })
+  })();
